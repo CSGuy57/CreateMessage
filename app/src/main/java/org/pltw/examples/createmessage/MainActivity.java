47971b155081f20a6f92ebbce48e9bfdcb4e5f8c
Daniel Lande <danlande@gmail.com>
2017-08-03 14:52:35 -0400

    Added comment

--- app/src/main/java/org/pltw/examples/createmessage/MainActivity.java
+++ app/src/main/java/org/pltw/examples/createmessage/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSubmitButton = (Button) findViewById(R.id.button_submit);
+        mSubmitButton = (Button) findViewById(R.id.button_submit); // comment
 
         mSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
