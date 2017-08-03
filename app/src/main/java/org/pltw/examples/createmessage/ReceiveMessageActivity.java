d08dd1861f795f6fafe97250b11867ba0e75d44a
Daniel Lande <danlande@gmail.com>
2017-08-03 15:56:18 -0400

    safds
    sdfsdf

--- app/src/main/java/org/pltw/examples/createmessage/ReceiveMessageActivity.java
+++ app/src/main/java/org/pltw/examples/createmessage/ReceiveMessageActivity.java
@@ -1,7 +1,7 @@
 package org.pltw.examples.createmessage;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.Intent;//comment
 import android.os.Bundle;
 import android.util.Log;
 
